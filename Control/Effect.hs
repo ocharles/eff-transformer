2da3972aaefbd5aefb54703ebcb50f9ffe2bed29
ElvishJerricco <elvishjerricco@gmail.com>
2016-04-08 05:49:37 -0400

    Fix freer monad implementation using free applicative.

--- Control/Effect.hs
+++ Control/Effect.hs
@@ -10,12 +10,33 @@ module Control.Effect
        ( -- $welcome
 
          -- * Core API
-         Eff(..), translate, run, Interprets, interpret, IsEff) where
+         Eff(..), Ap(..), translate, run, Interprets, interpret, IsEff) where
 
-import Control.Monad ((>=>))
 import Control.Monad.Morph
 import Data.Functor.Sum
 import GHC.Exts (Constraint)
+
+-- | The `Ap` Applicative is the Free applicative applied with `Sum`.
+-- The `Freer` monad is implemneted as the `Free` monad
+-- applied with `Lan Identity`.
+-- You can think of `Lan Identity` as a free functor.
+-- The free applicative trivially yields a free functor.
+-- Thus, applying this free applicative to `Free` should yield
+-- something similar to `Freer`.
+-- This is the approach in this module.
+-- By applying a free functor / applicative to `Free`,
+-- we get a `Freer` monad that supports interpretting applicative effects.
+-- By applying `Sum` to this freer monad, we get a freer monad transformer.
+data Ap f m a
+  = Pure a
+  | forall b. Ap f m (b -> a) :<*> Sum f m b
+instance Functor (Ap f m) where
+  fmap f (Pure a) = Pure (f a)
+  fmap f (xs :<*> x) = fmap (f .) xs :<*> x
+instance Applicative (Ap f m) where
+  pure = Pure
+  Pure f <*> y = fmap f y
+  (xs :<*> x) <*> y = (fmap flip xs <*> y) :<*> x
 
 -- | The 'Eff' monad transformer is used to write programs that require access to
 -- specific effects. In this library, effects are combined by stacking multiple
@@ -26,8 +47,18 @@ import GHC.Exts (Constraint)
 -- that 'Eff' transforms, which can itself be another instance of 'Eff'.
 data Eff f m a
   = Val a
-  | forall b. (Eff f m (b -> a)) :<*> (Sum f m b)
-  | forall b. (Sum f m b) :>>= (b -> Eff f m a)
+  | Eff (Ap f m (Eff f m a))
+instance Functor (Eff f m) where
+  fmap f (Val a) = Val (f a)
+  fmap f (Eff apA) = Eff $ fmap (fmap f) apA
+instance Applicative (Eff f m) where
+  pure = Val
+  Val f <*> a = fmap f a
+  Eff f <*> Eff a = Eff (fmap (<*>) f <*> a)
+  Eff f <*> Val a = Eff $ fmap (fmap ($ a)) f
+instance Monad (Eff f m) where
+  Val a >>= k = k a
+  Eff m >>= f = Eff ((>>= f) <$> m)
 
 -- | The 'IsEff' type family is used to make sure that a given monad stack
 -- is based around 'Eff'. This is important, as it allows us to reason about
@@ -52,14 +83,17 @@ translate step = run step'
     step' :: Sum f m x -> t m x
     step' (InL x) = step x
     step' (InR x) = lift x
+{-# INLINE translate #-}
+
+runAp :: Applicative g => (forall x. Sum f m x -> g x) -> Ap f m a -> g a
+runAp _ (Pure x) = pure x
+runAp u (x :<*> f) = flip id <$> u f <*> runAp u x
 
 run :: Monad t
     => (forall x. Sum f m x -> t x)
     -> Eff f m a -> t a
-run _    (Val a)    = return a
-run step (f :<*> a) = run step f <*> step a
-run step (a :>>= k) = step a >>= run step . k
-{-# INLINE translate #-}
+run _    (Val a)   = return a
+run step (Eff apA) = runAp step apA >>= run step
 
 -- | 'LiftProgram' defines an @mtl@-style type class for automatically lifting
 -- effects into 'Eff' stacks. When exporting libraries that you intend to
@@ -71,44 +105,24 @@ class (IsEff m, Monad m) => Interprets p m | m -> p where
   interpret :: p a -> m a
 
 instance Interprets f (Eff f m) where
-  interpret = (return id :<*>) . InL
+  interpret = Eff . (pure Val :<*>) . InL
   {-# INLINE interpret #-}
 
 instance (Monad m, Interprets f (Eff h m)) => Interprets f (Eff g (Eff h m)) where
   interpret = lift . interpret
   {-# INLINE interpret #-}
 
-instance Functor (Eff f m) where
-  fmap f (Val a) = Val (f a)
-  fmap f (g :<*> x) = fmap (f .) g :<*> x
-  fmap f (x :>>= k) = x :>>= (fmap f . k)
-  {-# INLINE fmap #-}
-
-instance Applicative (Eff f m) where
-  pure = Val
-  {-# INLINE pure #-}
-  Val f <*> a = fmap f a
-  (f :<*> x) <*> a = (flip <$> f <*> a) :<*> x
-  (x :>>= k) <*> a = x :>>= ((<*> a) . k)
-  {-# INLINE (<*>) #-}
-
-instance Monad (Eff f m) where
-  Val a >>= k = k a
-  (f :<*> x) >>= k = x :>>= \b -> fmap ($ b) f >>= k
-  (x :>>= k') >>= k = x :>>= (k' >=> k)
-  {-# INLINE (>>=) #-}
-
 instance MonadTrans (Eff f) where
-  lift = (return id :<*>) . InR
+  lift = Eff . (pure Val :<*>) . InR
   {-# INLINE lift #-}
 
 {- $welcome
 
 Welcome to @effect-interpreters@, a composable approach to managing effects in
 Haskell. @effect-interpreters@ is a small abstraction over the ideas of monad
-transformers, the @mtl@, and algebraic effects made popular through free monads
+transformers, the @mtl@, and algebraic effects made popular through Eff monads
 and various implementations of extensible effects. Rather than defining the
-whole program within a free monad and duplicating code on how to interpret
+whole program within a Eff monad and duplicating code on how to interpret
 well defined effects, this library leverages the tools of the monad transformer
 library to deliver something that is familiar, compatible with other libraries
 and as fast as vanilla monad transformers.
